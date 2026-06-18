2e46061ce80ce2a455872b1aa030fef04f832ae2
Mohammed Badr <badr290>
2026-06-19 00:35:54 +0100

    add c01

---
+++ c01/ex00/ft_ft.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void	ft_ft(int *nbr)
+{
+	*nbr = 42;
+}
+
+/*
+int	main(void)
+{
+	int	a;
+	int	*nbr;
+
+	a = 0;
+	nbr = &a;
+	ft_ft(nbr);
+	printf("%d", a);
+	return (0);
+}
+*/
